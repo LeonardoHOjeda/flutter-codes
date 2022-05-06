19022aedb24cfbe3e2fe0569baea0d8dca722e0f
Leonardo Ojeda <perros_rebeldes@hotmail.com>
2022-05-06 17:49:06 -0500

    Finalizacion seccion 11. Preferencias de usuario

--- preferences_app/lib/screens/home_screen.dart
+++ preferences_app/lib/screens/home_screen.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/share_preferences/preferences.dart';
 import 'package:preferences_app/widgets/widgets.dart';
 
-
 class HomeScreen extends StatelessWidget {
-
   static const String routerName = 'Home';
 
   const HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      drawer: SideMenu(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Modo Oscuro: '),
-          Divider(),
-          Text('Genero: '),
-          Divider(),
-          Text('Username: '),
-          Divider(),
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        drawer: SideMenu(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Modo Oscuro: ${Preferences.isDarkmode}'),
+            Divider(),
+            Text('Genero: ${Preferences.gender}'),
+            Divider(),
+            Text('Username: ${Preferences.name}'),
+            Divider(),
+          ],
+        ));
   }
 }
--- preferences_app/lib/screens/settings_screen.dart
+++ preferences_app/lib/screens/settings_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:preferences_app/providers/theme_provider.dart';
+import 'package:preferences_app/share_preferences/preferences.dart';
 import 'package:preferences_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class SettingsScreen extends StatefulWidget {
   static const String routerName = 'Settings';
@@ -12,9 +15,10 @@ class SettingsScreen extends StatefulWidget {
 
 class _SettingsScreenState extends State<SettingsScreen> {
 
-  bool isDarkmode = false;
-  int gender = 1;
-  String name = 'Pedro';
+  // bool isDarkmode = false;
+  // int gender = 1;
+  // String name = 'Pedro';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,41 +37,40 @@ class _SettingsScreenState extends State<SettingsScreen> {
                         TextStyle(fontSize: 45, fontWeight: FontWeight.w300)),
                 Divider(),
                 SwitchListTile(
-                    value: isDarkmode,
+                    value: Preferences.isDarkmode,
                     title: Text('Darkmode'),
                     onChanged: (value) {
-                      isDarkmode = value;
-                      setState(() {
-                        
-                      });
+                      Preferences.isDarkmode = value;
+                      final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+                      value ? themeProvider.setDarkMode() : themeProvider.setLightMode();
+                      setState(() {});
                     }),
                 Divider(),
                 RadioListTile<int>(
                     value: 1,
-                    groupValue: gender,
+                    groupValue: Preferences.gender,
                     title: Text('Masculino'),
                     onChanged: (value) {
-                      gender = value ?? 1;
+                      Preferences.gender = value ?? 1;
                       setState(() {});
                     }),
                 Divider(),
                 RadioListTile<int>(
                     value: 2,
-                    groupValue: gender,
+                    groupValue: Preferences.gender,
                     title: Text('Femenino'),
                     onChanged: (value) {
-                      gender = value ?? 2;
+                      Preferences.gender = value ?? 2;
                       setState(() {});
                     }),
                 Divider(),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: TextFormField(
-                    initialValue: 'Leonardo',
+                    initialValue: Preferences.name,
                     onChanged: (value){
-                      name = value;
-                      setState(() {
-                      });
+                      Preferences.name = value;
+                      setState(() {});
                     },
                     decoration: InputDecoration(
                       labelText: 'Nombre',
